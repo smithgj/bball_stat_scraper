cf747b5f316b155613448282b344f1539ce21819
Greg Smith <smithgj66@hotmail.com>
2021-05-02 16:24:02 -0500

    Add files via upload

--- getBattingStats.py
+++ getBattingStats.py
@@ -3,8 +3,8 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://atl-02.statsplus.net/tlg/reports/news/html/players/player_36536.html"
-#url = "https://atl-02.statsplus.net/tlg/reports/news/html/players/player_57423.html"
+#url = "https://atl-02.statsplus.net/tlg/reports/news/html/players/player_36536.html"
+url = "https://atl-02.statsplus.net/tlg/reports/news/html/players/player_57423.html"
 
 
 batter_page = requests.get(url)
@@ -23,7 +23,7 @@ one_liner = one_liner.find('a')
 one_liner = one_liner.text
 # RF AARON 'ALL RISE' JUDGE #99 - AGE: 29 - BATS: R - THROWS: R - MORALE: VERY GOOD
 # C TRENT JACKSON #5 - AGE: 21 - BATS: L - THROWS: R - MORALE: GOOD
-print(one_liner)
+# print(one_liner)
 one_liner_list = one_liner.split()
 pos = one_liner_list[0]
 i = 1
@@ -32,9 +32,32 @@ while not one_liner_list[i].startswith('#'):
     name = name + one_liner_list[i] + ' '
     i = i + 1
 name = name.strip()
-print(name)
+# print(name)
 for x in one_liner_list:
     if x.startswith("#"):
         number = x[1:]
 
+# print(number)
+
+age = one_liner_list[one_liner_list.index('AGE:') + 1]
+bats = one_liner_list[one_liner_list.index('BATS:') + 1]
+throws = one_liner_list[one_liner_list.index('THROWS:') + 1]
+morale_first = one_liner_list.index('MORALE:') + 1
+morale_range = range(morale_first, len(one_liner_list))
+# print(morale_range)
+morale=''
+for n in morale_range:
+    morale = morale + one_liner_list[n] + ' '
+morale = morale.strip()
+
+print(team)
+print(pos)
+print(name)
 print(number)
+print(age)
+print(bats)
+print(throws)
+print(morale)
+
+### should be able to use the above for pitchers as well
+
---
+++ getPitchingStats.py
@@ -0,0 +1,59 @@
+import sys
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://atl-02.statsplus.net/tlg/reports/news/html/leagues/league_153_players.html"
+base_url = "https://atl-02.statsplus.net/tlg/reports/news/html"
+
+start_page = requests.get(url)
+# print(page)
+soup = BeautifulSoup(start_page.content, 'html.parser')
+last_name_pages = soup.find('td', {'class':'boxtitle'})
+# print(last_name_pages)  <a href="../leagues/league_153_players_b.html">B</a>
+
+# gets rid of extra lines between valid anchors:
+last_name_hrefs = last_name_pages.find_all('a')
+# print(type(last_name_hrefs))  #it is a ResultSet
+# print(last_name_hrefs[3]) <a href="../leagues/league_153_players_d.html">D</a>
+
+#urls for player pages A thru Z:
+player_pages_urls = []
+
+# the following yields 26 lines like: ../leagues/league_153_players_w.html
+#  so we will strip off the ".." and prepend the base_url
+for a in last_name_hrefs:
+#    print(a['href'])
+    player_page_end_url = a['href'].strip('.')
+    #print(player_page_end_url)
+    player_pages_urls.append(base_url + player_page_end_url)
+
+players = []
+for page in player_pages_urls:
+    # print(page)
+    player_page = requests.get(page)
+    player_list_soup = BeautifulSoup(player_page.content, 'html.parser')
+    all_anchors = player_list_soup.find('table', {'class':'data sortable'})
+    all_hrefs = all_anchors.find_all('a')
+    for b in all_hrefs:
+        page_end_url = b['href'].strip('.')
+        if "players" in page_end_url:
+            players.append(base_url + page_end_url)
+
+print('total number of players found = ' + str(len(players)))
+
+# debug to print out all urls of individual player pages
+#with open('player_pages.txt', 'w') as f:
+#    for i in players:
+#        print(i, file=f)
+
+
+# for each url in the players link list:
+    # extract_data(url)
+
+#def extract_data(url):
+    #get page
+    #if position player
+        #extract position data
+    #else
+        #extract pitcher data
+    #put in db
