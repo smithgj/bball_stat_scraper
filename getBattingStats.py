5fdf3801a73f788d7874da7ff4552a54785d2a6d
Greg Smith <smithgj66@hotmail.com>
2021-05-02 23:29:29 -0500

    Add files via upload

--- getBattingStats.py
+++ getBattingStats.py
@@ -61,3 +61,89 @@ print(morale)
 
 ### should be able to use the above for pitchers as well
 
+batting_ratings = soup.find('td', string='Contact')
+# print(batting_ratings)
+skip1 = batting_ratings.find_next('td')
+skip2 = skip1.find_next('td')
+contact_overall_td = skip2.find_next('td')
+contact_overall = contact_overall_td.text
+contact_v_left_td = contact_overall_td.find_next('td')
+contact_v_left = contact_v_left_td.text
+contact_v_right_td = contact_v_left_td.find_next('td')
+contact_v_right = contact_v_right_td.text
+contact_potential_td = contact_v_right_td.find_next('td')
+contact_potential = contact_potential_td.text
+
+print(contact_overall)
+print(contact_v_left)
+print(contact_v_right)
+print(contact_potential)
+
+batting_ratings = soup.find('td', string='Gap')
+# print(batting_ratings)
+skip1 = batting_ratings.find_next('td')
+skip2 = skip1.find_next('td')
+gap_overall_td = skip2.find_next('td')
+gap_overall = gap_overall_td.text
+gap_v_left_td = gap_overall_td.find_next('td')
+gap_v_left = gap_v_left_td.text
+gap_v_right_td = gap_v_left_td.find_next('td')
+gap_v_right = gap_v_right_td.text
+gap_potential_td = gap_v_right_td.find_next('td')
+gap_potential = gap_potential_td.text
+
+print(gap_overall)
+print(gap_v_left)
+print(gap_v_right)
+print(gap_potential)
+
+batting_ratings = soup.find('td', string='Power')
+skip1 = batting_ratings.find_next('td')
+skip2 = skip1.find_next('td')
+power_overall_td = skip2.find_next('td')
+power_overall = power_overall_td.text
+power_v_left_td = power_overall_td.find_next('td')
+power_v_left = power_v_left_td.text
+power_v_right_td = power_v_left_td.find_next('td')
+power_v_right = power_v_right_td.text
+power_potential_td = power_v_right_td.find_next('td')
+power_potential = power_potential_td.text
+
+print(power_overall)
+print(power_v_left)
+print(power_v_right)
+print(power_potential)
+
+batting_ratings = soup.find('td', string='Eye')
+skip1 = batting_ratings.find_next('td')
+skip2 = skip1.find_next('td')
+eye_overall_td = skip2.find_next('td')
+eye_overall = eye_overall_td.text
+eye_v_left_td = eye_overall_td.find_next('td')
+eye_v_left = eye_v_left_td.text
+eye_v_right_td = eye_v_left_td.find_next('td')
+eye_v_right = eye_v_right_td.text
+eye_potential_td = eye_v_right_td.find_next('td')
+eye_potential = eye_potential_td.text
+
+print(eye_overall)
+print(eye_v_left)
+print(eye_v_right)
+print(eye_potential)
+
+batting_ratings = soup.find('td', string="Avoid K's")
+skip1 = batting_ratings.find_next('td')
+skip2 = skip1.find_next('td')
+avoidK_overall_td = skip2.find_next('td')
+avoidK_overall = avoidK_overall_td.text
+avoidK_v_left_td = avoidK_overall_td.find_next('td')
+avoidK_v_left = avoidK_v_left_td.text
+avoidK_v_right_td = avoidK_v_left_td.find_next('td')
+avoidK_v_right = avoidK_v_right_td.text
+avoidK_potential_td = avoidK_v_right_td.find_next('td')
+avoidK_potential = avoidK_potential_td.text
+
+print(avoidK_overall)
+print(avoidK_v_left)
+print(avoidK_v_right)
+print(avoidK_potential)
